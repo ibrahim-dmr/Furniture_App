c3eaff24cf5e8f2c03265e7b845d8365f518da4a
ibrahim-dmr <ibrahimdmr1103@gmail.com>
2024-04-05 11:58:33 +0300

    file paths updated

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import Login from "./src/screens/Login"
+import Signup from "./src/screens/Signup"
+import {NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  return(
+    <NavigationContainer>
+
+      <Login/>
+      <Signup/>
+
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
